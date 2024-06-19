a1da1b903854d49e1af7e2b8be72082eb2345c86
Voron <mcbleshned@gmail.com>
2024-06-19 13:58:30 +0500

    Add inline keyboard for genres

--- keyboards/kb_genres.py
+++ keyboards/kb_genres.py
@@ -1,5 +1,18 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from lexicon.lexicon import LEXICON_GENRES
 
 
-def genres_kb():
-    pass
+def genres_kb() -> InlineKeyboardMarkup:
+    kb_builder = InlineKeyboardBuilder()
+    buttons: list[InlineKeyboardButton] = []
+
+    for button, text in LEXICON_GENRES.items():
+        buttons.append(InlineKeyboardButton(
+            text=text,
+            callback_data=button
+        ))
+    
+    kb_builder.row(*buttons, width=4)
+
+    return kb_builder.as_markup()
