fffc69d71775948d3073b52dcf424a64b25dc6f7
Voron <mcbleshned@gmail.com>
2024-06-19 12:00:26 +0500

    fix: add LEXICON_GENRES and fix my_filter

--- filters/my_filters.py
+++ filters/my_filters.py
@@ -1,15 +1,11 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import CallbackQuery
-
-
-genres: list[str] = ['action', 'drama', 'comedy', 'mystery', 'melodrama', 'fantasy',
-                     'biography', 'humor', 'horror', 'adventure', 'science fiction', 
-                     'history', 'thriller', 'western', 'romance', 'anime']
+from lexicon.lexicon import LEXICON_GENRES
 
 
 class IsGenre(BaseFilter):
     def __init__(self):
-         self.genres = genres
+         self.genres = LEXICON_GENRES
     
     def __call__(self, message: CallbackQuery) -> bool | dict[str, str]:
-        return {genre: message.data} if message.data in genres else True
+        return {genre: message.data} if message.data in self.genres else False
