cf9dd8b95ff2ca0d2199d860d487aefdd4cf4577
Voron <mcbleshned@gmail.com>
2024-06-20 16:42:40 +0500

    Completed api for movie_search command

--- api/api_kinopoisk.py
+++ api/api_kinopoisk.py
@@ -1,8 +1,16 @@
 import requests
+from config_data.config import API_KEY
+
+
+url = 'https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit={}&query={} {}'
+headers = {
+    "accept": "application/json",
+    "X-API-KEY": API_KEY
+}
 
 
 def movie_search(name: str, genre: str, limit: int):
-    pass
+    return requests.get(url=url.format(limit, name, genre), headers=headers).json()
 
 
 def movie_by_rating():
