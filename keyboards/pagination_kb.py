e5f6f580359606716a99ed70282207afe0e91871
Voron <mcbleshned@gmail.com>
2024-06-21 19:09:51 +0500

    Add wrapper on pagination_kb function

--- keyboards/pagination_kb.py
+++ keyboards/pagination_kb.py
@@ -3,10 +3,19 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 from lexicon.lexicon import LEXICON
 
 
-def create_pagination_keyboard(*buttons: str) -> InlineKeyboardMarkup:
+def pagination_keyboard(*buttons: str) -> InlineKeyboardMarkup:
     kb_builder = InlineKeyboardBuilder()
     kb_builder.row(*[InlineKeyboardButton(
         text=LEXICON[button] if button in LEXICON else button,
         callback_data=button) for button in buttons]
     )
     return kb_builder.as_markup()
+
+def create_pagination_keyboard(page: int, limit: int):
+    middle_button = f'{page}/{limit}'
+    if page == 1:
+        return pagination_keyboard(middle_button, 'forward')
+    elif 1 < page < limit:
+        return pagination_keyboard('backward', middle_button, 'forward')
+    else:
+        return pagination_keyboard('backward', middle_button)
