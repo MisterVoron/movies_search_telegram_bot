85051fc8fe7ac448d9875b456b0e7ef49445bc11
Voron <mcbleshned@gmail.com>
2024-06-26 15:14:48 +0500

    Add whole script of handlers for movie_by_rating command

--- filters/my_filters.py
+++ filters/my_filters.py
@@ -1,8 +1,14 @@
 from aiogram.filters import BaseFilter
-from aiogram.types import CallbackQuery
+from aiogram.types import CallbackQuery, Message
 from lexicon.lexicon import LEXICON_GENRES
 
 
 class IsGenre(BaseFilter):
     async def __call__(self, callback: CallbackQuery) -> bool | dict[str, str]:
         return {'genre': LEXICON_GENRES[callback.data]} if callback.data in LEXICON_GENRES else False
+
+
+class IsRating(BaseFilter):
+    async def __call__(self, message: Message) -> bool:
+        return message.text.isdigit() or ''.join(map(str.strip, test.split('-'))).replace('.', '').isdigit()
+        
--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -4,11 +4,11 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state
 from aiogram.types import Message, CallbackQuery
 from lexicon.lexicon import LEXICON
-from states.states import FSMSearchScript
-from filters.my_filters import IsGenre
+from states.states import FSMSearchScript, FSMSearchScriptRating
+from filters.my_filters import IsGenre, IsRating
 from keyboards.kb_genres import genres_kb
 from keyboards.pagination_kb import create_pagination_keyboard
-from api.api_kinopoisk import movie_search
+from api.api_kinopoisk import movie_search, movie_by_rating
 from database.db import users
 from utils.utils import convert_list_in_string, photo_caption
 import requests
@@ -49,7 +49,7 @@ async def process_command_cancel_in_state(message: Message, state: FSMContext):
 @router.message(Command(commands='movie_search'), StateFilter(default_state))    
 async def process_command_movie_search(message: Message, state: FSMContext):
     await message.answer(
-        text=LEXICON['movie_search']
+        text=LEXICON['/movie_search']
     )
     await state.set_state(FSMSearchScript.name)
 
@@ -58,7 +58,7 @@ async def process_command_movie_search(message: Message, state: FSMContext):
 async def process_name_sent(message: Message, state: FSMContext):
     await state.update_data(name=message.text)
     await message.answer(
-        text=LEXICON['script_answer_1'],
+        text=LEXICON['genres'],
         reply_markup=genres_kb()
     )
     await state.set_state(FSMSearchScript.genre)
@@ -76,7 +76,7 @@ async def process_genre_sent(callback: CallbackQuery, state: FSMContext, genre: 
     await state.update_data(genre=genre)
     await callback.message.delete()
     await callback.message.answer(
-        text=LEXICON['script_answer_2']
+        text=LEXICON['limit']
     )
     await state.set_state(FSMSearchScript.limit)
 
@@ -112,6 +112,13 @@ async def process_limit_sent(message: Message, state: FSMContext, bot: Bot):
                              ))
 
 
+@router.message(StateFilter(FSMSearchScript.limit))
+async def warning_not_genre(message: Message):
+    await message.answer(
+        text=LEXICON['warning_limit']
+    )
+
+
 @router.callback_query(F.data.in_({'backward', 'forward'}))
 async def process_backward_forward_press(callback: CallbackQuery, bot: Bot):
     if callback.data == 'backward':
@@ -130,11 +137,52 @@ async def process_backward_forward_press(callback: CallbackQuery, bot: Bot):
     await callback.answer()
 
 
-
+@router.message(Command(commands='movie_by_rating'), StateFilter(default_state))
+async def process_movie_by_rating_command(message: Message, state: FSMContext):
+    await message.answer(
+        text=LEXICON['/movie_by_rating']
+    )
+    await state.set_state(FSMSearchScriptRating.rating)
     
 
+@router.message(StateFilter(FSMSearchScriptRating.rating), IsRating())
+async def process_rating_sent(message: Message, state: FSMContext):
+    await state.update_data(rating=message.text)
+    await message.answer(
+        text=LEXICON['limit']
+    )
+    await state.set_state(FSMSearchScriptRating.limit)
 
 
+@router.message(StateFilter(FSMSearchScriptRating.rating))
+async def warning_not_rating(message: Message):
+    await message.answer(
+        text=LEXICON['warning_rating']
+    )
+
+
+@router.message(StateFilter(FSMSearchScriptRating.limit), F.text.isdigit())
+async def process_limit_sent(message: Message, state: FSMContext, bot: Bot):
+    await state.update_data(limit=int(message.text))
+    user_id = message.from_user.id
+    users[user_id] = await state.get_data()
+    await state.clear()
+    info = movie_by_rating(rating=users[user_id]['rating'],
+                        limit=users[user_id]['limit'])
+    movie = info['docs'][0]
+    if users[user_id]['limit'] == 1:
+        caption = photo_caption(movie)
+        await bot.send_photo(message.chat.id, movie['poster']['url'], caption=caption, parse_mode='HTML')
+    else:
+        users[user_id]['position'] = 0
+        users[user_id]['movies'] = info['docs']
+        caption = photo_caption(movie)
+        await bot.send_photo(message.chat.id, movie['poster']['url'],
+                             caption=caption, parse_mode='HTML',
+                             reply_markup=create_pagination_keyboard(
+                                page=users[message.from_user.id]['position'],
+                                limit=users[message.from_user.id]['limit']
+                             ))
 
 
 
